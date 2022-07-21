4c32431e0a3b20d742a4ca9a4cea723cda5ca1c3
tharunamireddy <108273258+tharunamireddy@users.noreply.github.com>
2022-07-21 18:54:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://nikithagundla.github.io/Identifying-Disease-in-Tea-Leaf/',
+        url:'https://www.nyckel.com/v1/functions/zqeeot07egy24asg/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
